a8fe63a203ef4d5ac6605d83b9d1783c0c0ed32d
rocrill <crillyr@tcd.ie>
2022-06-04 11:27:34 +0000

    Initial deploy - step 3.

--- velo_city/settings.py
+++ velo_city/settings.py
@@ -22,11 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*cnqqd3coa2i3o6nv59i7%m&)+4gt%m=@)9bhf$^&3$dmp%wu%'
+SECRET_KEY = os.environ.get(SECRET_KEY, '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+DEBUG = 'DEVELOPMENT' in os.environ
 ALLOWED_HOSTS = ['velo-city-rc.herokuapp.com', 'localhost']
 
 
