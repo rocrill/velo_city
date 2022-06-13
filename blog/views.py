34d81a84fd02f00ab2826c54e4234f4bbc9f3bad
rocrill <crillyr@tcd.ie>
2022-06-13 16:51:34 +0000

    Add initial code for post_list.html blog template.

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from .models import Post
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
-    template_name = 'blog.html'
+    template_name = 'post_list.html'
 
 class PostDetail(generic.DetailView):
     model = Post
