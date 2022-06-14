c9e6fd25a1d161cd2c9867e29aad2bff877c5394
rocrill <crillyr@tcd.ie>
2022-06-14 10:14:26 +0000

    Add basic comment model for blog posts #32.

--- blog/models.py
+++ blog/models.py
@@ -21,3 +21,22 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment (models.Model):
+    """Model for comments under blog posts."""
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="comments")
+    name = models.CharField(max_length=80)
+    email = models.EmailField(blank=True)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
